7cef4e06657b40d146b0c681569e71edda1df567
codeasaglacier <remingtonshock@gmail.com>
2019-05-21 14:57:13 -0800

    fixed nav bleedover, changed button background

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const mouseoverPurple = document.querySelectorAll('.nav a');
+mouseoverPurple.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.stopPropagation();
+        event.target.style.color = "purple";
+        setTimeout(function () {
+            event.target.style.color = "";
+
+        }, 2000)
+    }, false)
+});
+
+const btnClick = document.querySelectorAll('div .btn');
+btnClick.forEach(element => {
+    element.addEventListener('click', event2 => {
+        event2.target.style.backgroundColor = "purple";
+    })
+})
