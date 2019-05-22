73971aec10a773a6a2337009dd3052aa050a8913
codeasaglacier <remingtonshock@gmail.com>
2019-05-21 16:20:01 -0800

    added mouseEnter and mouseOut

--- js/index.js
+++ js/index.js
@@ -4,16 +4,27 @@ mouseoverPurple.forEach(element => {
     element.addEventListener('mouseover', event => {
         event.stopPropagation();
         event.target.style.color = "purple";
-        setTimeout(function () {
-            event.target.style.color = "";
-
-        }, 2000)
-    }, false)
+        setTimeout(()=> event.target.style.color = "", 2000)
+    })
 });
+
+const introMouseEnter = document.querySelectorAll('.intro');
+introMouseEnter.forEach(element => {
+    element.addEventListener('mouseenter', event => {
+        event.target.style.color = "green"})});
+
+const introMouseOut = document.querySelectorAll('.intro');
+introMouseOut.forEach(element => {
+    element.addEventListener('mouseleave', event => {
+        event.target.style.color = "purple";
+        setTimeout(()=> event.target.style.color = "", 2000)
+    })});
 
 const btnClick = document.querySelectorAll('div .btn');
 btnClick.forEach(element => {
     element.addEventListener('click', event2 => {
         event2.target.style.backgroundColor = "purple";
+        setTimeout(()=> event2.target.style.backgroundColor = ""
+        , 2000)
     })
 })
